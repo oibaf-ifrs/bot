4b74338bec0ff897ba0ef2ec3a9b15677d82d2f6
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2021-06-30 01:48:30 -0300

    Initial commit
    Initial commit

---
+++ src/main/java/edu/ifrs/bot/App.java
@@ -0,0 +1,14 @@
+package edu.ifrs.bot;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        OrionBot bot = new OrionBot();
+        // bot.start();
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/edu/ifrs/bot/commands/Command.java
@@ -0,0 +1,7 @@
+package edu.ifrs.bot.commands;
+
+public interface Command {
+
+    public void execute();
+
+}
